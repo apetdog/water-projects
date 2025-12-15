932a544f69ad03410e5905ddfc98449f57a83adf
orienslu <logeast@outlook.com>
2025-12-15 23:09:30 +0800

    build: update vite base path for deployment

--- 01-digital-twin-park/vite.config.js
+++ 01-digital-twin-park/vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: './',
+  base: '/twin-park/',
   server: {
     port: 9001,
   }
